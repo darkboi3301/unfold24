da5b2875b1c1bfae2832729a87524aa201eaefd4
J-Sanjana <sanjanaj1506@gmail.com>
2024-12-01 19:48:58 +0530

    resolve height error

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,6 +1,14 @@
-export default  function Home() {
-    return (
-        <>
-        Hello</>
-    )
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='bg-black w-screen h-screen'>
+      <nav className="h-10 w-screen background-blur backdrop-brightness-50 z-50 text-white p-5 flex flex-row justify-stretch">
+        <ul>LOGO</ul>
+        <ul>DROPSERVE</ul>
+      </nav>
+    </div>
+  )
 }
+
+export default Home
